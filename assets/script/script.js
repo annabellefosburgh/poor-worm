fe7148cd3ac2a5564c60f0d3d7b80e183758d61a
annabellefosburgh <afosburgh20@gmail.com>
2023-07-27 15:26:00 -0500

    Defining variables

---
+++ assets/script/script.js
@@ -0,0 +1,26 @@
+//Assigning variables to the different sections that act as pages
+var welcome = document.querySelector("#introduction");
+var intro = document.querySelector("#intro");
+var question = document.querySelector("#question");
+var highScore = document.querySelector("#highscore");
+var askQuestion = document.querySelector("#ask-question");
+var scoreBoard = document.querySelector("#submit");
+
+//Assigning variables for all buttons
+var startBtn = document.querySelector("#start-button");
+var allButtons = document.querySelectorAll(".choices");
+var answer1 = document.querySelector("#answer1");
+var answer2 = document.querySelector("#answer2");
+var answer3 = document.querySelector("#answer3");
+var answer4 = document.querySelector("#answer4");
+var backButton = document.querySelector("#back-btn");
+var clearButton= document.querySelector("#clear-btn");
+var submitButton = document.querySelector("#submit-btn");
+
+//Assigning variables that are used for local storage/scores/etc
+var checkLine = document.querySelector("#check-line");
+var finalScore = document.querySelector("#final-score");
+var userInitial = document.querySelector("#initial");
+var scoreRecord = document.querySelector("#score-record");
+var scoreCheck = document.querySelector("#score-check");
+var finish = document.querySelector("#finish");
