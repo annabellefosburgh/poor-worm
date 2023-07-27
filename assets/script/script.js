4462bda6f73c83f3b4be7be4495c53086fdb6273
annabellefosburgh <afosburgh20@gmail.com>
2023-07-27 15:37:15 -0500

    Writing questions

--- assets/script/script.js
+++ assets/script/script.js
@@ -24,3 +24,34 @@ var userInitial = document.querySelector("#initial");
 var scoreRecord = document.querySelector("#score-record");
 var scoreCheck = document.querySelector("#score-check");
 var finish = document.querySelector("#finish");
+
+//Writing the questions and their proper answers as an object
+var questionObject = [
+    {
+        question: "Question 1 : Commonly used data types DO NOT include:",
+        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        answer: "c"
+    },
+    {
+        question: "Question 2 : The condition of an if / else statement must be enclosed with ____.",
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "b"
+    },
+    {
+        question: "Question 3 : An array in javascript can be used to store ____.",
+        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+        answer: "d"
+    },
+    {
+        question: "Question 4 : String values must be closed with a ____ when being assigned to variables.",
+        choices: ["a. comma", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "c"
+    },
+    {
+        question: "Question 5 : A very useful tool for development and debugging for printing content to the debugger is: ",
+        choices: ["a. JavaScript", "b. terminal/bash", "c. for loops", "d. console.log"],
+        answer: "d"
+    },
+];
+
+//
